32407314547600549ac186b3abfd24cf173fd1ea
Kevin <FER260418@spengergasse.at>
2026-09-23 21:03:20 +0200

    get/set Methoden hinzugefügt

--- Laptop.java
+++ Laptop.java
@@ -3,4 +3,34 @@ public class Laptop
     private String hersteller;
     private int kosten;
     private boolean neu;
+    
+    public void setHersteller(String neuHersteller)
+    {
+        hersteller = neuHersteller;
+    }
+    
+    public void setKosten(int neuKosten)
+    {
+        kosten = neuKosten;
+    }
+    
+    public void setNeu(boolean neuNeu)
+    {
+        neu = neuNeu;
+    }
+    
+    public String getHersteller()
+    {
+        return hersteller;
+    }
+    
+    public int getKosten()
+    {
+        return kosten;
+    }
+    
+    public boolean getNeu()
+    {
+        return neu;
+    }
 }
